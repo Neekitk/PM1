db146da69ff70090752c0a8363ddb6c56d56185a
Nikita <nikstriker64@gmail.com>
2025-10-06 16:21:34 +0300

    Добавлен класс Program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using GameSimulators;
+
+class Program
+{
+    static void Main()
+    {
+        GameSimulator game = new GameSimulator();
+        game.StartGame();
+    }
+}
